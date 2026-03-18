17209091e6037a8d41522da564d3582f96a5421e
Pablo Carmo <pablohsouzacarmo@gmail.com>
2026-03-18 00:05:04 -0300

    Adição de método para mapear a página login criada com o Thymeleaf

---
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -0,0 +1,13 @@
+package med.voll.web_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping
+    public String carregaPaginaListagem(){
+        //Método para mapear e carregar a página de login já criada
+        return "autenticacao/login";
+    }
+}
