08b113b5cd895d83aec3bf444c0f3a082b06b6a9
Pablo Carmo <pablohsouzacarmo@gmail.com>
2026-03-18 00:42:08 -0300

    Correção do mapeamento de /login

--- src/main/java/med/voll/web_application/controller/LoginController.java
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping
+    @GetMapping ("/login")
     public String carregaPaginaListagem(){
         //Método para mapear e carregar a página de login já criada
         return "autenticacao/login";
